cace1d546153fcda615cc6c13c740a6268941ab1
gifaldyazkaa <falcxxdev@pm.me>
2022-02-22 12:53:11 +0700

    feat: transform source script to typescript

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import simpleGit from "simple-git";
+import jsonfile from "jsonfile";
+import random from "random";
+import moment from "moment";
+const FILE_PATH: string = "../data/data.json";
+
+const makeCommit = (n) => {
+  if (n === 0) return simpleGit().push();
+  console.log(`\n├─ Commit ${n}\n│`);
+  const x: number = random.int(0, 54);
+  const y: number = random.int(0, 6);
+  const DATE: string = moment()
+    .subtract(0.9, "y")
+    .add(0, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const formattedDate: string = moment(DATE).format("L");
+
+  const data = {
+    date: formattedDate,
+  };
+
+  console.log(`└─ Creating commit at : ${formattedDate}...`);
+
+  jsonfile.writeFile(FILE_PATH, data, () => {
+    simpleGit()
+      .add([FILE_PATH])
+      .commit(
+        `feat: create commit at ${formattedDate}`,
+        { "--date": DATE },
+        makeCommit.bind(this, --n)
+      )
+      .push();
+  });
+};
+
+makeCommit(700);
